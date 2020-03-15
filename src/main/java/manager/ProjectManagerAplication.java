f3e74c15184fe7853be27132a8164e304c987db0
Haineala Emese <haineala_magdalena1@yahoo.com>
2020-03-15 18:43:48 +0200

    Added getMapping method and topBar.html file

--- src/main/java/manager/ProjectManagerAplication.java
+++ src/main/java/manager/ProjectManagerAplication.java
@@ -3,7 +3,7 @@ package manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 public class ProjectManagerAplication 
 {
 	public static void main(String[] args) {
