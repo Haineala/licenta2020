95ce5dfa246632e233b466f66d1a6aa1973ba5d1
Haineala Emese <haineala_magdalena1@yahoo.com>
2020-03-15 17:07:17 +0200

    Added Join Table to AppUser class

--- src/main/java/manager/entity/AppUser.java
+++ src/main/java/manager/entity/AppUser.java
@@ -1,10 +1,16 @@
 package manager.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -30,6 +36,13 @@ public class AppUser {
 	
 	@Column(name="enabled", nullable = false)
 	private boolean enabled;
+	
+	@ManyToMany(cascade=CascadeType.ALL)
+	@JoinTable(name="user_role",
+			joinColumns= {@JoinColumn(name="user_id")},
+			inverseJoinColumns= {@JoinColumn(name="role_id")})
+	private List<Role> roles;
+
 	
 	
 	public AppUser(Integer id, String firstName, String lastName, String email, String password, boolean enabled) {
