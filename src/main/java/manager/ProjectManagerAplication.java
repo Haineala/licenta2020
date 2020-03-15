b21172f53dd5c2790210603326e4fef6b4f3bf17
Haineala Emese <haineala_magdalena1@yahoo.com>
2020-03-15 15:24:23 +0200

    Added main class

---
+++ src/main/java/manager/ProjectManagerAplication.java
@@ -0,0 +1,13 @@
+package manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerAplication 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerAplication.class, args);
+	}
+
+}
