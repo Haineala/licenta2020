e0dc6d44753449c3b425f235e25fbf74c7f92301
Haineala Emese <haineala_magdalena1@yahoo.com>
2020-03-23 15:20:17 +0200

    Added LoggedOutController

---
+++ src/main/java/manager/controller/LoggedOutController.java
@@ -0,0 +1,16 @@
+package manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoggedOutController {
+	
+	@GetMapping("/logOut")
+	public String getLoggedOutView(Model model)
+	{
+		return "views/LogOutView";
+	}
+
+}
